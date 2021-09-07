20f834a0de38c9b06461c823da7080a5b56fabbe
Atilla <you@example.com>
2021-09-07 11:02:53 +0200

    i added the folder that has screenshots of calculations

--- hallo.py
+++ hallo.py
@@ -1,10 +1,10 @@
-print ('Hello wereld van…')                        
-print('    pppp    y     y   ttttttt   h   h    oooo   nnnn')     
-print('    p   p    y   y       t      h   h   o    o  n   n')
-print('    p   p     y y        t      h   h   o    o  n   n')
-print('    pppp       y         t      hhhhh   o    o  n   n')
-print('    p          y         t      h   h   o    o  n   n')
-print('    p          y         t      h   h   o    o  n   n')
-print('    p          y         t      h   h    oooo   n   n')
+print ('Hello wereld van…'                        
+print('    pppp    y     y   ttttttt   h   h    oooo   nnnn)     
+print('    p   p    y   y       t      h   h   o    o  n   n');
+printer('    p   p     y y        t      h   h   o    o  n   n')
+echo('    pppp       y         t      hhhhh   o    o  n   n')
+pront('    p          y         t      h   h   o    o  n   n')
+print('    p          y         t(if)      h   h   o    o  n   n')
+print('    p          y         t print('this code works perfectly!')     h   h    oooo   n   n')
 
-# Atilla Correia Nunes 99059374
+@ Atilla Correia Nunes 99059374
